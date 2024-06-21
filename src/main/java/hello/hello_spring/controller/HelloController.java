1215529e3e9b8501984680ad0b4345cc4da8c977
soyoon0309 <soyoon0309@naver.com>
2024-06-22 03:07:06 +0900

    7장 API 관련 code 생성 HelloController.java 내부에 class 추가
    ResponseBody 사용
    hello-string -> 응답 body부에 문자를 직접 지정하겠다 (xml방식 tag없이)
    hello-api
    객체를 넘겨서 json방식으로 return
    getter setter는 alt + insert key를 이용해 생성 가능

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model; //잘못 지정 시 addAttribute X
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,31 @@ public class HelloController {
         model.addAttribute("name", name); //param val이 name이므로, attribute name을 name으로
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "Hello " + name; //응답 body부에 직접 넣겠다 / tag도 없이 그대로 문자만 적힘 ex) hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 넘김 (key)
+    }
+
+    static class Hello{
+        private String name;
+        //getter setter 의 경우 alt + insert key를 눌러 바로 setting 가능
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
