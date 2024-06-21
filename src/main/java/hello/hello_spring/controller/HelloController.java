56a06135789f0ea94e4447f58eace92ebcac4f52
soyoon0309 <soyoon0309@naver.com>
2024-06-22 01:55:26 +0900

    6장 MVC와 템플릿 엔진 관련 source HelloController.java에서 hello-mvc Mapping 결과 hello-template.html 실행함

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.ui.Model; //잘못 지정 시 addAttribute X
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello !!");
         return "hello"; //hello.html 실행
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name); //param val이 name이므로, attribute name을 name으로
+        return "hello-template";
     }
 }
